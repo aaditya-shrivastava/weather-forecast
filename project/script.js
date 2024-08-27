b395181c964eec2f927d2dc03df331962291edb6
Aaditya-0406 <aadityashrivastava2004@gmail.com>
2024-08-27 23:33:56 +0530

    update

--- project/script.js
+++ project/script.js
@@ -1,22 +1,24 @@
 const options = {
-    method: 'GET',
-	headers: {
-        'x-rapidapi-key': 'YOUR_API_KEY',
-		'x-rapidapi-host': 'the-weather-api.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "YOUR_API_KEY",
+    "x-rapidapi-host": "yahoo-weather5.p.rapidapi.com",
+  },
 };
+
 async function getWeatherForecast() {
-    const BaseUrl = 'https://the-weather-api.p.rapidapi.com/api/weather';
-    let city = document.getElementById("inputCity").value;
-    const url = `${BaseUrl}/${city}`;
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        const data = result.data;
-        document.getElementById("BGimg").src = result.data.bg_image
-        console.log(data);
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    let location = document.getElementById("inputCity").value;
+    const url = `https://yahoo-weather5.p.rapidapi.com/weather?location=${location}&format=json&u=f`;
+    const response = await fetch(url, options);
+    const result = await response.json();
+    document.getElementById("wind").innerText = `${result.current_observation.wind.speed} km/hr`;
+    document.getElementById("temp").innerText = `${((result.current_observation.condition.temperature - 32) * 5/9).toFixed(2)} °C`;
+    document.getElementById("cloud").innerText = result.current_observation.condition.text;
+    document.getElementById("humidity").innerText = result.current_observation.atmosphere.humidity;
+  } catch (error) {
+    console.error(error);
+  }
 }
-document.getElementById("btn").addEventListener('click', getWeatherForecast);
+
+document.getElementById("btn").addEventListener("click", getWeatherForecast);
